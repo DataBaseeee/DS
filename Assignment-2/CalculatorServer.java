4dc302f38c77a30ecba328efc82cea66857b43a9
Dhanu0407 <ghadagedhanashri95@gmail.com>
2025-05-06 23:57:02 +0530

    as-2

---
+++ Assignment-2/CalculatorServer.java
@@ -0,0 +1,33 @@
+import CalculatorApp.*;
+import org.omg.CORBA.*;
+import org.omg.PortableServer.*;
+import org.omg.PortableServer.POA;
+import java.util.Properties;
+
+public class CalculatorServer {
+    public static void main(String args[]) {
+        try {
+            ORB orb = ORB.init(args, null);
+            POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+            rootpoa.the_POAManager().activate();
+
+            CalculatorImpl calcImpl = new CalculatorImpl();
+            calcImpl.setORB(orb);
+
+            org.omg.CORBA.Object ref = rootpoa.servant_to_reference(calcImpl);
+            Calculator href = CalculatorHelper.narrow(ref);
+
+            org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+
+            String name = "Calculator";
+            NameComponent path[] = ncRef.to_name(name);
+            ncRef.rebind(path, href);
+
+            System.out.println("Calculator Server ready...");
+            orb.run();
+        } catch (Exception e) {
+            System.err.println("ERROR: " + e);
+        }
+    }
+}
