ed20a820c46095d09fbabb1d03cee186f4250099
Dhanu0407 <ghadagedhanashri95@gmail.com>
2025-05-06 11:05:54 +0530

    all

---
+++ DS_1C.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class DS_1C{
+
+    static int a = 10;
+    static int b = 20;
+    static int x = a+b;
+
+    public static void main(String[]args){
+        System.out.println(x);
+    }
+}
